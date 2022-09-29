1bef49e11535fc21e66e4eafffeb81a1fc6318bb
JamesW825 <jamesg06wilson@gmail.com>
2022-09-29 12:22:11 +0000

    added the questions

---
+++ MultipleChoiceQuiz.py
@@ -0,0 +1,14 @@
+# This is a general knowledge, 10 question, multiple choice quiz.
+
+Questions=[["1. What is the biggest ocean?", "2. What is roughly the speed of sound?(mph/kph)"],
+["3. What was the first country to use tanks in combat during WWI?", "4. Which of these ground animals are the fastest?"],
+["5. ", "6. "],
+["7. ", "8. "],
+["9. ", "10. "],
+Options=[["A-Arctic", "B-Indian", "C-Pacific", "D-Atlantic"],["A-330/500", "B-770/1200", "C-1200/2000", "D-2000,3200"]]
+Answers=["1. C-Pacific", "2. B-770/1200", "3. D-Britain", "4. A-Cheetah",]
+
+def 10Q_Quiz():
+score = 0
+n=0
+for n <= 10
