31f96bf4c494a8ed0032014ebfc8b17e42c2a11d
JamesW825 <jamesg06wilson@gmail.com>
2022-11-17 14:04:27 +0000

    ...

--- Date Function.py
+++ Date Function.py
@@ -3,7 +3,7 @@
 def date():
     #Months=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     Wdate=input("Enter a date in a worded from. (Eg: 1st Jan 2022 - Including spaces)\n")
-    Year=strWdate[-4:]
+    Year=str(Wdate[-4:])
     Month=Wdate[4:],Wdate[-4:]
     Day=Wdate[4:]
     print("Date is: ", Day, Month, Year)
