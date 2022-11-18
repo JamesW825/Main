6c0ed95878215e08b8c411ca02f334a543a96298
JamesW825 <jamesg06wilson@gmail.com>
2022-11-18 11:16:12 +0000

    Getting there...

--- AlexMall. program/AlexMal.py
+++ AlexMall. program/AlexMal.py
@@ -1,5 +1,18 @@
 # This program is to store the amount of times alex is 'negative' in chemistry alone.
 
-Total=0
 Day_Entry=input("Enter amount of times Alex was 'negative' in Lesson: \n")
-f=open("AlexEnteries.txt")
+Date=input("Enter date of the lesson: (DD/MM/YY)\n")
+f=open("AlexEnteries.txt", "a")
+f.write(Day_Entry), (" - "), (Date)
+f.write('\n')
+f.close()
+
+Total=0
+Myfile='AlexEnteries.txt'
+f=open("AlexEnteries.txt", "r")
+with open(Myfile) as f:
+    for n in f:
+        n=n.strip()
+        Total=Total+int(n)
+print("Current total:",Total)
+f.close()
