e6cf23a91894549b4dd9b670af7ae51102673b07
JamesW825 <jamesg06wilson@gmail.com>
2022-11-18 10:10:52 +0000

    ...

--- AlexMal.py
+++
@@ -1,3 +0,0 @@
-# This program is to store the amount of times alex is 'negative' in chemistry alone.
-
-Times=[]
---
+++ AlexMall. program/AlexMal.py
@@ -0,0 +1,5 @@
+# This program is to store the amount of times alex is 'negative' in chemistry alone.
+
+Total=0
+Day_Entry=input("Enter amount of times Alex was 'negative' in Lesson: \n")
+f=open("AlexEnteries.txt")
