877e0de5de98c156107e29caef240dc26a6cb1e0
JamesW825 <jamesg06wilson@gmail.com>
2022-12-07 11:05:21 +0000

    ...

--- VSV Questions/3RecursionQ's.py
+++ VSV Questions/3RecursionQ's.py
@@ -13,4 +13,16 @@ n=int(input("Enter any even number: \n"))
 SumNum(n)
 
 # 2.
-def 
+def FibWORec():
+  a=int(input("Enter the first number of desired series: \n"))
+  b=int(input("Enter the second number of desired series: \n"))
+  x=int(input("Enter the number of terms needed: \n"))
+  while(x-2):
+    c=a+b
+    a=b
+    b=c
+    print(c,end=" ")
+    x=x-1
+ 
+# 3.
+def FibWRec():
