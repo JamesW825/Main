b19a601db12051e497cc438bae82ffbdd35f3187
JamesW825 <jamesg06wilson@gmail.com>
2022-12-15 09:27:04 +0000

    ....

--- VSV Questions/3RecursionQ's.py
+++ VSV Questions/3RecursionQ's.py
@@ -41,7 +41,7 @@ def fib(n):
 
 def fibonacci2(n):
     fibNumbers = [0,1]  #list of first two Fibonacci numbers
-	# now append the sum of the two previous numbers to the list    
+	# Now append the sum of the two previous numbers to the list    
     for i = 2 to n:
         fibNumbers.append(fibNumbers[i-1] + fibNumbers[i-2])
 	      next i
