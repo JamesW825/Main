c9b4a606e6bec9ece33013af26e4d6f6447e455c
JamesW825 <jamesg06wilson@gmail.com>
2023-07-07 21:43:58 +0000

    turtle stuff

--- Turtle.py
+++ Turtle.py
@@ -1,3 +1,10 @@
 # This is my attempt at doing a turtle program.
 
 import turtle
+
+s = turtle.getscreen()
+t = turtle.Turtle()
+turtle.title("My Turtle Program")
+t.pendown()
+t.right(90)
+t.penup()
